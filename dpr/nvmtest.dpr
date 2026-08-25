e1b7f1e32bfef2f79908d854e21e4c0e1d80f3c6
Nicholas Smith <47601940+NickyNockyNu@users.noreply.github.com>
2026-08-25 20:51:10 +0100

    cli test harness

--- dpr/nvmtest.dpr
+++ dpr/nvmtest.dpr
@@ -1,23 +1,9 @@
 program nvmtest;
 
 {$APPTYPE CONSOLE}
-{$R *.res}
 
 uses
-  System.Classes,
-  NixVM.Core.Strings in '..\src\NixVM.Core.Strings.pas',
-  NixVM.Core.CPU in '..\src\NixVM.Core.CPU.pas',
-  NixVM.Core.Instructions in '..\src\NixVM.Core.Instructions.pas',
-  NixVM.Core.Memory in '..\src\NixVM.Core.Memory.pas',
-  NixVM.Core.Registers in '..\src\NixVM.Core.Registers.pas',
-  NixVM.Core.System in '..\src\NixVM.Core.System.pas',
-  NixVM.Harness in '..\src\NixVM.Harness.pas',
-  NixVM.Harness.Timing in '..\src\NixVM.Harness.Timing.pas',
-  NixVM.Harness.Window in '..\src\NixVM.Harness.Window.pas',
-  NixVM.Tools.IR in '..\src\NixVM.Tools.IR.pas',
-  NixVM.Tools.Disasm in '..\src\NixVM.Tools.Disasm.pas',
-  NixVM.Tools.Assembler in '..\src\NixVM.Tools.Assembler.pas',
-  NixVM.Core.ROM in '..\src\NixVM.Core.ROM.pas';
+  NixVM.Harness;
 
 type
   TTestSystemMemory = packed record
